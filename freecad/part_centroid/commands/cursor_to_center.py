e9ad60abc04dc6b1acdb51d2051590d3089fa3b8
Jonathan E. Railsback <jonbitzen@hotmail.com>
2023-07-24 19:26:17 -0400

    implemented function to move the cursor to the selection centroid

--- freecad/part_centroid/commands/cursor_to_center.py
+++ freecad/part_centroid/commands/cursor_to_center.py
@@ -1,5 +1,5 @@
-import FreeCAD
-import FreeCADGui
+import FreeCAD as App
+import FreeCADGui as Gui
 
 
 class CursorToCenterCommand(object):
@@ -15,16 +15,37 @@ class CursorToCenterCommand(object):
         }
     
     def IsActive(self):
-        if (FreeCAD.ActiveDocument is not None):
+        if (App.ActiveDocument is not None):
             return True
         return False
     
     def Activated(self):
-        print("Do something nice here")
-        # OK so we want to:
-        #   - get the selection, and create a list of objects and subobjects that we're going
-        #     to add to a compound that we will use to get a box centroid
-        #     - if the selected object has valid selected subobjects, add those to the compound,
-        #       otherwise, just add the whole object
-        #     - once we're done adding objects / subobjects, get the compound bbox, get its
-        #   - move the cursor to the computed center
+
+        selected_objects = Gui.Selection.getSelectionEx()
+
+        # remove the cursor, we dont use it to find a center point        
+        selected_objects = \
+            [sel_obj for sel_obj in selected_objects if sel_obj.ObjectName != "centroid_cursor" ]
+
+        count = 0
+        centroid = App.Vector(0,0,0)
+
+        for selected_object in selected_objects:
+            # if we have one or more sub objects selected, iterate over it and
+            # add it to the 
+            if len(selected_object.SubObjects) > 0:
+                for sub_obj in selected_object.SubObjects:
+                    count += 1
+                    centroid = centroid + sub_obj.BoundBox.Center
+            else:
+                count += 1
+                centroid = centroid + selected_object.Object.BoundBox.Center
+
+        centroid = centroid/count
+
+        cursor = App.ActiveDocument.getObject("centroid_cursor")
+        if cursor is None:
+            return
+        
+        cursor.Placement.Base = centroid
+        
